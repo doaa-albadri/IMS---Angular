76e97a7ba822e698a352c15b0d7940e599c98cd2
Doaa Albadri <doaabadri13@gmail.com>
2023-03-19 04:26:10 -0700

    remove header & sidebar from login and add only to home for now

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,9 +3,11 @@ import { Component, Output } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  sidebarExpanded = true;
+
   cardData: { title: string; data: number }[] = [
     { title: 'Products', data: 5 },
     { title: 'Orders', data: 10 },

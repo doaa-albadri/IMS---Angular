f9a65f968def291c64c7fbc8f69eb1b531e671f4
Doaa Albadri <doaabadri13@gmail.com>
2023-03-20 05:41:00 -0700

    fetch card data for api

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,31 @@
-import { Component, Output } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   sidebarExpanded = true;
 
-  cardData: { title: string; data: number }[] = [
-    { title: 'Products', data: 5 },
-    { title: 'Orders', data: 10 },
-    { title: 'Suppliers', data: 15 },
-  ];
+  cardData: { title: string; stat: number }[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.fetchCardData();
+  }
+
+  fetchCardData = () => {
+    this.apiService.fetchData().subscribe(
+      (res: any) => {
+        this.cardData = res['-NQyvmjiAa_QIg4GYWpo'];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
 }

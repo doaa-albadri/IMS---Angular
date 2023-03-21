c00e688aa4614289585475d87e1a943cccde9feb
Doaa Albadri <doaabadri13@gmail.com>
2023-03-21 05:45:04 -0700

    implement get orders http req

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,11 +6,43 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class ApiService {
+  // ordersData: { title: string; stat: number }[] = [];
   constructor(private http: HttpClient) {}
 
-  fetchData() {
+  fetchStats() {
     return this.http.get(
       'https://ng-project-c984a-default-rtdb.firebaseio.com/stats.json'
     );
   }
+
+  fetchOrdersData() {
+    return this.http.get(
+      'https://ng-project-c984a-default-rtdb.firebaseio.com/orders.json'
+    );
+    // .subscribe(
+    //   (res: any) => {
+    //     this.ordersData = res['-NQz3JFXxx7JeleMIRr9'];
+    //     // console.log('orders results', this.ordersData);
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // )
+  }
+
+  // addData() {
+  //   this.http
+  //     .post<{ name: string }>(
+  //       'https://ng-project-c984a-default-rtdb.firebaseio.com/orders.json',
+  //       [
+  //         { title: 'Delivered', stat: 83 },
+  //         { title: 'Pending', stat: 50 },
+  //         { title: 'Delivered', stat: 11 },
+  //       ],
+  //       { observe: 'response' }
+  //     )
+  //     .subscribe((resData) => {
+  //       console.log(resData);
+  //     });
+  // }
 }

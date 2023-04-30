ceff640555b04aca66b8dfce28436fb42629e02f
Doaa Albadri <doaabadri13@gmail.com>
2023-04-30 02:06:08 -0700

    remove hover events

--- src/app/components/bar-chart/bar-chart.component.ts
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -41,40 +41,4 @@ export class BarChartComponent {
       { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
     ],
   };
-
-  // events
-  public chartClicked({
-    event,
-    active,
-  }: {
-    event?: ChartEvent;
-    active?: {}[];
-  }): void {
-    // console.log(event, active);
-  }
-
-  public chartHovered({
-    event,
-    active,
-  }: {
-    event?: ChartEvent;
-    active?: {}[];
-  }): void {
-    // console.log(event, active);
-  }
-
-  public randomize(): void {
-    // Only Change 3 values
-    this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      Math.round(Math.random() * 100),
-      56,
-      Math.round(Math.random() * 100),
-      40,
-    ];
-
-    this.chart?.update();
-  }
 }

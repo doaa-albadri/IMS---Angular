f5b5704d60edfbd3872bb78fe7560f4e100fae91
Doaa Albadri <doaabadri13@gmail.com>
2023-04-30 02:08:24 -0700

    work on data using rxjs and observable

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
 import DatalabelsPlugin from 'chartjs-plugin-datalabels';
-import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { ChartConfiguration, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import { ApiService } from 'src/app/services/api.service';
+import { map } from 'rxjs/operators';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-pie-chart',
@@ -9,155 +12,47 @@ import { BaseChartDirective } from 'ng2-charts';
   styleUrls: ['./pie-chart.component.scss'],
 })
 export class PieChartComponent {
-  @Input() data!: { title: string; stat: number }[];
+  private _apiService = inject(ApiService);
+
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
+  chartData$ = this._apiService
+    .fetchOrdersData()
+    .pipe(map((res: any) => res['-NQz3JFXxx7JeleMIRr9']));
+
+  labels$ = this.chartData$.pipe(
+    map((labels: any) => labels.map((item: any) => item.title))
+  );
+
+  stats$ = this.chartData$.pipe(
+    map((stats: any) => stats.map((item: any) => item.stat))
+  );
+
+  combinedData$ = combineLatest([this.labels$, this.stats$]);
+
   // Pie
-  public pieChartOptions: ChartConfiguration['options'] = {
+
+  pieChartOptions: ChartConfiguration['options'] = {
     responsive: true,
     plugins: {
       legend: {
         display: true,
         position: 'top',
       },
-      datalabels: {
-        formatter: (value, ctx) => {
-          if (ctx.chart.data.labels) {
-            return ctx.chart.data.labels[ctx.dataIndex];
-          }
-        },
-      },
     },
   };
 
-  public pieChartData: ChartData<'pie', number[], string | string[]> = {
-    labels: ['Delivered', 'Pending', 'Cancelled'],
-    datasets: [
-      {
-        data: [500, 50, 100],
-      },
-    ],
-  };
-  public pieChartType: ChartType = 'pie';
-  public pieChartPlugins = [DatalabelsPlugin];
-
-  // events
-  public chartClicked({
-    event,
-    active,
-  }: {
-    event: ChartEvent;
-    active: {}[];
-  }): void {
-    // console.log(event, active);
+  getPieChartData(chartData: any) {
+    return {
+      labels: chartData[0],
+      datasets: [
+        {
+          data: chartData[1],
+        },
+      ],
+    };
   }
 
-  public chartHovered({
-    event,
-    active,
-  }: {
-    event: ChartEvent;
-    active: {}[];
-  }): void {
-    // console.log(event, active);
-  }
-
-  changeLabels(): void {
-    const words = [
-      'hen',
-      'variable',
-      'embryo',
-      'instal',
-      'pleasant',
-      'physical',
-      'bomber',
-      'army',
-      'add',
-      'film',
-      'conductor',
-      'comfortable',
-      'flourish',
-      'establish',
-      'circumstance',
-      'chimney',
-      'crack',
-      'hall',
-      'energy',
-      'treat',
-      'window',
-      'shareholder',
-      'division',
-      'disk',
-      'temptation',
-      'chord',
-      'left',
-      'hospital',
-      'beef',
-      'patrol',
-      'satisfied',
-      'academy',
-      'acceptance',
-      'ivory',
-      'aquarium',
-      'building',
-      'store',
-      'replace',
-      'language',
-      'redeem',
-      'honest',
-      'intention',
-      'silk',
-      'opera',
-      'sleep',
-      'innocent',
-      'ignore',
-      'suite',
-      'applaud',
-      'funny',
-    ];
-    const randomWord = () => words[Math.trunc(Math.random() * words.length)];
-    this.pieChartData.labels = new Array(3).map((_) => randomWord());
-
-    this.chart?.update();
-  }
-
-  addSlice(): void {
-    if (this.pieChartData.labels) {
-      this.pieChartData.labels.push(['Line 1', 'Line 2', 'Line 3']);
-    }
-
-    this.pieChartData.datasets[0].data.push(400);
-
-    this.chart?.update();
-  }
-
-  removeSlice(): void {
-    if (this.pieChartData.labels) {
-      this.pieChartData.labels.pop();
-    }
-
-    this.pieChartData.datasets[0].data.pop();
-
-    this.chart?.update();
-  }
-
-  changeLegendPosition(): void {
-    if (this.pieChartOptions?.plugins?.legend) {
-      this.pieChartOptions.plugins.legend.position =
-        this.pieChartOptions.plugins.legend.position === 'left'
-          ? 'top'
-          : 'left';
-    }
-
-    this.chart?.render();
-  }
-
-  toggleLegend(): void {
-    if (this.pieChartOptions?.plugins?.legend) {
-      this.pieChartOptions.plugins.legend.display =
-        !this.pieChartOptions.plugins.legend.display;
-    }
-
-    this.chart?.render();
-  }
+  pieChartType: ChartType = 'pie';
+  pieChartPlugins = [DatalabelsPlugin];
 }

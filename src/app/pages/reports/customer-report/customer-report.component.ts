9f74b18942bbb83802131a64420c935a0f61a286
Doaa Albadri <doaabadri13@gmail.com>
2023-05-24 05:33:38 -0700

    create new components for reports page

---
+++ src/app/pages/reports/customer-report/customer-report.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-report',
+  templateUrl: './customer-report.component.html',
+  styleUrls: ['./customer-report.component.css']
+})
+export class CustomerReportComponent {
+
+}

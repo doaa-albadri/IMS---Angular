1e9ad1eececa58465b4989f5e3141b7f1db691ff
Doaa Albadri <doaabadri13@gmail.com>
2023-05-28 04:11:53 -0700

    add download pdf functionality

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
@@ -8,4 +9,17 @@ import { Component } from '@angular/core';
 export class ReportsComponent {
   sidebarExpanded = true;
   selectedReport: string = 'sales';
+
+  downloadPDF() {
+    const table: HTMLElement | any = document.getElementById('report');
+
+    html2canvas(table).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      var imgWidth = 208;
+      var imgHeight = (canvas.height * imgWidth) / canvas.width;
+      pdf.addImage(imgData, 'PNG', 5, 5, imgWidth, imgHeight);
+      pdf.save('report.pdf');
+    });
+  }
 }

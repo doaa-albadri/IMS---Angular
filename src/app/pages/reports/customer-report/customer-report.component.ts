d7c4cf46c642af7b1efc15640f7aa8b6bc1d4a63
Doaa Albadri <doaabadri13@gmail.com>
2023-06-13 05:36:28 -0700

    start creating customer repost

--- src/app/pages/reports/customer-report/customer-report.component.ts
+++ src/app/pages/reports/customer-report/customer-report.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-customer-report',
   templateUrl: './customer-report.component.html',
-  styleUrls: ['./customer-report.component.css']
+  styleUrls: ['./customer-report.component.css'],
 })
 export class CustomerReportComponent {
+  data = [
+    {
+      title: 'Customer Satisfaction:',
+      stat: '100%',
+    },
+    { title: 'Number of Orders:', stat: 1000 },
+    { title: 'Commission:', stat: 500 },
+    { title: 'Returned Customers:', stat: 50 },
+  ];
 
+  constructor(private apiService: ApiService) {}
+
+  pieChartData$ = this.apiService
+    .fetchSalesTypesData()
+    .pipe(map((res: any) => res));
+
+  pieLabels$ = this.pieChartData$.pipe(
+    map((labels: any) => labels.map((item: any) => item.title))
+  );
+
+  pieStats$ = this.pieChartData$.pipe(
+    map((stats: any) => stats.map((item: any) => item.stat))
+  );
+
+  combinedPieData$ = combineLatest([this.pieLabels$, this.pieStats$]);
 }

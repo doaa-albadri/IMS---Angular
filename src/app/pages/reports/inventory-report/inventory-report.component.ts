09e62f20b5445584cb9b237374bf0418b88e15e6
Doaa Albadri <doaabadri13@gmail.com>
2023-06-19 01:53:56 -0700

    create inventory report

--- src/app/pages/reports/inventory-report/inventory-report.component.ts
+++ src/app/pages/reports/inventory-report/inventory-report.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { ApiService } from 'src/app/services/api.service';
+interface CardData {
+  title: string;
+  stat: number | string;
+}
 
+interface PurchaseDetail {
+  vendor: string;
+  category: string;
+  product: string;
+  qnty: number;
+  productRate: number;
+  sellRate: number;
+}
 @Component({
   selector: 'app-inventory-report',
   templateUrl: './inventory-report.component.html',
-  styleUrls: ['./inventory-report.component.css']
+  styleUrls: ['./inventory-report.component.css'],
 })
 export class InventoryReportComponent {
+  purchaseData: PurchaseDetail[];
 
+  // purchaseDetails: PurchaseDetail[] = [
+  //   {
+  //     vendor: 'first vendor',
+  //     category: 'first cat',
+  //     product: 'first product',
+  //     qnty: 10,
+  //     productRate: 33,
+  //     sellRate: 44,
+  //   },
+  //   {
+  //     vendor: 'first vendor',
+  //     category: 'first cat',
+  //     product: 'first product',
+  //     qnty: 10,
+  //     productRate: 33,
+  //     sellRate: 44,
+  //   },
+  //   {
+  //     vendor: 'first vendor',
+  //     category: 'first cat',
+  //     product: 'first product',
+  //     qnty: 10,
+  //     productRate: 33,
+  //     sellRate: 44,
+  //   },
+  //   {
+  //     vendor: 'first vendor',
+  //     category: 'first cat',
+  //     product: 'first product',
+  //     qnty: 10,
+  //     productRate: 33,
+  //     sellRate: 44,
+  //   },
+  //   {
+  //     vendor: 'first vendor',
+  //     category: 'first cat',
+  //     product: 'first product',
+  //     qnty: 10,
+  //     productRate: 33,
+  //     sellRate: 44,
+  //   },
+  // ];
+
+  constructor(private apiService: ApiService) {}
+
+  cardData$: CardData[] | any = this.apiService
+    .fetchInventoryStats()
+    .pipe(map((res: any) => res));
+
+  purchasesData$: PurchaseDetail[] | any = this.apiService
+    .fetchPurchases()
+    .pipe(map((res: any) => res));
+
+  barChartData$ = this.apiService
+    .fetchRevenueData()
+    .pipe(map((res: any) => res));
 }
